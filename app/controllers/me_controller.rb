03fa50fa0e1a030962ce11840b692d065a23e984
romain gille <gille.romain@yahoo.fr>
2022-02-20 17:00:48 +0100

    homepage begun with json file for datas

--- app/controllers/me_controller.rb
+++ app/controllers/me_controller.rb
@@ -1,4 +1,9 @@
+require 'json'
+
 class MeController < ApplicationController
   def home
+    filepath = 'lib/assets/me.json'
+    serialized_me = File.read(filepath)
+    @me = JSON.parse(serialized_me)
   end
 end
