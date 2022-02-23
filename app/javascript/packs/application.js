c374b020fcb7937fdbbb44a20058c934dc9e65b9
romain gille <gille.romain@yahoo.fr>
2022-02-23 22:24:02 +0100

    bootstrap cdn js added

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,10 +2,9 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+import Rails from "@rails/ujs";
+import Turbolinks from "turbolinks";
+import "channels";
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import "channels"
-
-Rails.start()
-Turbolinks.start()
+Rails.start();
+Turbolinks.start();
