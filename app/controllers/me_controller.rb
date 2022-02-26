95b10e6fb04b1ae2ce0c10b485cee6fa450a93e7
romain gille <gille.romain@yahoo.fr>
2022-02-26 14:26:58 +0100

    gallery imgs added and gallery json done

--- app/controllers/me_controller.rb
+++ app/controllers/me_controller.rb
@@ -6,4 +6,13 @@ class MeController < ApplicationController
     serialized_me = File.read(filepath)
     @me = JSON.parse(serialized_me)
   end
+
+  def gallery
+    filepath = 'lib/assets/images-gallery.json'
+    serialized_me = File.read(filepath)
+    @gallery1 = JSON.parse(serialized_me)
+    filepath = 'lib/assets/images-gallery-2.json'
+    serialized_me = File.read(filepath)
+    @gallery2 = JSON.parse(serialized_me)
+  end
 end
