b880be1c5b7974e290d425f590dca555132d1b48
romain gille <gille.romain@yahoo.fr>
2022-03-11 14:29:48 +0100

    turbolinks blocked on gallery link

--- app/javascript/controllers/gallery_controller.js
+++ app/javascript/controllers/gallery_controller.js
@@ -8,6 +8,15 @@ export default class extends Controller {
     console.log("hey");
     // Fancybox.bind('[data-fancybox="Gallery1"]', {});
     // Fancybox.bind('[data-fancybox="Gallery2"]', {});
+    setTimeout(100);
+    Fancybox.bind(".gallery-images a", {
+      groupAll: true, // Group all items
+      on: {
+        ready: (fancybox) => {
+          console.log(`fancybox #${fancybox.id} is ready!`);
+        },
+      },
+    });
   }
 
   displayimages() {
