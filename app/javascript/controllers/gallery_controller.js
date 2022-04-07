9d3813813100ab6e4390ce3fbccd19a4404786d8
romain gille <gille.romain@yahoo.fr>
2022-04-07 10:47:07 +0200

    added possibility to select images

--- app/javascript/controllers/gallery_controller.js
+++ app/javascript/controllers/gallery_controller.js
@@ -2,12 +2,10 @@ import { Controller } from "@hotwired/stimulus";
 import { Fancybox } from "@fancyapps/ui";
 
 export default class extends Controller {
-  static targets = ["button", "images", "arrow"];
+  static targets = ["button", "images", "arrow", "SelectButton", "selectImage"];
 
   connect() {
     console.log("hey");
-    // Fancybox.bind('[data-fancybox="Gallery1"]', {});
-    // Fancybox.bind('[data-fancybox="Gallery2"]', {});
     setTimeout(100);
     Fancybox.bind(".gallery-images a", {
       groupAll: true, // Group all items
@@ -19,16 +17,51 @@ export default class extends Controller {
     });
   }
 
-  displayimages() {
+  displaySelectButton(e) {
+    e.preventDefault();
+
+
+    console.log(this.selectImageTargets);
+    if (this.selectImageTarget.classList.contains("d-none")) {
+      this.selectImageTargets.forEach((e) => {
+        console.log(e);
+        e.classList.remove("d-none")
+      })
+    } else {
+      this.selectImageTargets.forEach((e) => {
+        console.log(e);
+        e.classList.add("d-none")
+        e.style.background = null
+      })
+    }
+
+
+  }
+
+  displayimages(event) {
     event.preventDefault();
     if (this.imagesTarget.classList.contains("d-none")) {
       this.imagesTarget.classList.remove("d-none");
       this.arrowTarget.classList.remove("fa-angle-down");
       this.arrowTarget.classList.add("fa-angle-up");
+      this.SelectButtonTarget.classList.remove("d-none");
+
     } else {
       this.imagesTarget.classList.add("d-none");
       this.arrowTarget.classList.add("fa-angle-down");
       this.arrowTarget.classList.remove("fa-angle-up");
+      this.SelectButtonTarget.classList.add("d-none");
+    }
+  }
+  selectImage(e) {
+
+    e.preventDefault();
+    console.log(e.currentTarget);
+    if (e.currentTarget.style.background.search("green") != -1) {
+      e.currentTarget.style.background = null
+      console.log("yoo");
+    } else {
+      e.currentTarget.style.background = "green"
     }
   }
 }
