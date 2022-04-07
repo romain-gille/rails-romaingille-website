0e2a0a8c6479a976b4924a132ccda417820922e8
romain gille <gille.romain@yahoo.fr>
2022-04-07 15:48:57 +0200

    download files button added

--- app/javascript/controllers/gallery_controller.js
+++ app/javascript/controllers/gallery_controller.js
@@ -2,9 +2,10 @@ import { Controller } from "@hotwired/stimulus";
 import { Fancybox } from "@fancyapps/ui";
 
 export default class extends Controller {
-  static targets = ["button", "images", "arrow", "SelectButton", "selectImage"];
-
+  static targets = ["button", "images", "arrow", "SelectButton", "selectImage", "DownloadButton"];
+  static variables = { numImagesSelected: Number }
   connect() {
+    this.numImagesSelectedValue = 0
     console.log("hey");
     setTimeout(100);
     Fancybox.bind(".gallery-images a", {
@@ -17,22 +18,29 @@ export default class extends Controller {
     });
   }
 
+  DownloadButton() {
+    console.log("download");
+  }
   displaySelectButton(e) {
     e.preventDefault();
-
-
     console.log(this.selectImageTargets);
     if (this.selectImageTarget.classList.contains("d-none")) {
       this.selectImageTargets.forEach((e) => {
         console.log(e);
         e.classList.remove("d-none")
+
       })
+      this.SelectButtonTarget.innerHTML = "Unselect all"
+      this.DownloadButtonTarget.classList.remove("d-none");
     } else {
       this.selectImageTargets.forEach((e) => {
         console.log(e);
         e.classList.add("d-none")
         e.style.background = null
+
       })
+      this.SelectButtonTarget.innerHTML = "Select images"
+      this.DownloadButtonTarget.classList.add("d-none");
     }
 
 
@@ -54,14 +62,16 @@ export default class extends Controller {
     }
   }
   selectImage(e) {
-
     e.preventDefault();
     console.log(e.currentTarget);
     if (e.currentTarget.style.background.search("green") != -1) {
       e.currentTarget.style.background = null
-      console.log("yoo");
+      this.numImagesSelectedValue -= 1
     } else {
       e.currentTarget.style.background = "green"
+      this.numImagesSelectedValue += 1
     }
+    console.log(this.numImagesSelectedValue);
+    this.DownloadButtonTarget.innerHTML = `Download ${this.numImagesSelectedValue} files`
   }
 }
